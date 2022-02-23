c63ed2e5fbed293b9a4e2df31440df4d5e04b31b
Edoardo Lai <edoardolai96@gmail.com>
2022-02-23 15:49:45 +0100

    Stilizzazione logo e header

---
+++ src/components/myHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+      <div class="logo_box">
+          <img src="../assets/img/spotify-1.svg" alt="">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "myHeader",
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .header{
+        height: 100px;
+        background-color: #303A45;
+        .logo_box{
+            height: 100%;
+            padding: 10px;
+            img{
+                height: 100%;
+            }
+        }
+    }
+</style>
